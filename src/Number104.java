27326a789c51779dfd5bedd3980e097e4bce6e8d
tangpeng <a12345678>
2021-04-04 13:44:32 +0800

    Initial commit

---
+++ src/Number104.java
@@ -0,0 +1,16 @@
+
+/**
+ * 二叉树的最大深度
+ */
+public class Number104 {
+
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
