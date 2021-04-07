b1dfe114d1b362196f525ddde9df82d8d65f7fb0
tangpeng <a12345678>
2021-04-07 21:31:10 +0800

    检查是否是一颗对称二叉树

---
+++ src/leetCode/Number101.java
@@ -0,0 +1,29 @@
+package leetCode;
+
+
+/**
+ * 检查是否是一颗对称二叉树
+ */
+public class Number101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        } else {
+            return isSame(root.left, root.right);
+        }
+    }
+
+    private boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) { // 有一个为空
+            return true;
+        } else if (left != null && right != null) {
+            if (left.value == right.value) {
+                return isSame(left.left, left.right) && isSame(right.left, right.right);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
